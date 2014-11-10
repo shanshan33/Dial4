ef7b569cf9c5d1bbca15a6f4d514f27aa74c5677
Shanshan ZHAO <chezzhaoss@gmail.com>
2014-11-11 00:06:08 +0100

    Initial Commit

---
+++ Dial4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dial4
+//
+//  Created by Shanshan ZHAO on 11/11/14.
+//  Copyright (c) 2014 Shanshan ZHAO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dial4/DetailViewController.m
@@ -0,0 +1,46 @@
+//
+//  DetailViewController.m
+//  Dial4
+//
+//  Created by Shanshan ZHAO on 11/11/14.
+//  Copyright (c) 2014 Shanshan ZHAO. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+#pragma mark - Managing the detail item
+
+- (void)setDetailItem:(id)newDetailItem {
+    if (_detailItem != newDetailItem) {
+        _detailItem = newDetailItem;
+            
+        // Update the view.
+        [self configureView];
+    }
+}
+
+- (void)configureView {
+    // Update the user interface for the detail item.
+    if (self.detailItem) {
+        self.detailDescriptionLabel.text = [self.detailItem description];
+    }
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    [self configureView];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
